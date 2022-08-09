8d7e5e299ccfe23ed117b13a5211456802269b44
Sid <siddiqkaithodu007@gmail.com>
2022-08-09 02:57:52 +0000

    Add files via upload
    handling of unused form

---
+++ assets/js/contact.js
@@ -0,0 +1,5 @@
+function checker(){ 
+  let m=document.querySelector("#form_error");
+  m.innerHTML="Ouch.I chose to Close the messaging system due to some maintance issues(or should i say i dont know how to set up one)";
+  return false;
+  }  
